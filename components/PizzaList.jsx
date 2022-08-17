bbd929806fd282fb2ad467443d75a0a628b5855e
Bhavikt5 <91686940+Bhavikt5@users.noreply.github.com>
2022-08-17 14:11:13 +0530

    initial commit

---
+++ components/PizzaList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "../styles/PizzaList.module.css";
+import PizzaCard from "./PizzaCard";
+
+const PizzaList = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        minima cupiditate unde natus repudiandae perferendis, corporis et
+        molestiae dolore sequi labore officiis iusto voluptas ullam?
+      </p>
+      <div className={styles.wrapper}>
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+      </div>
+    </div>
+  );
+};
+
+export default PizzaList;
